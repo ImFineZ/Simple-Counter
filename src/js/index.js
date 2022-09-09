546ced7b72f603b8b6dc22dc5e729057709e9c71
ImFineZ <greivinrodriguez@icloud.com>
2022-09-09 20:17:14 +0000

    Mi primer contador

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,41 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+//import "../component/icons.js";
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import PropTypes from "prop-types";
 
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const SimpleCounter = (props) => {
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
+        <i className="fa-regular fa-clock"></i>
+      </div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
+    </div>
+  );
+};
+SimpleCounter.prototypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
+};
+let counter = 0;
+setInterval(function () {
+  const four = Math.floor(counter / 1000);
+  const three = Math.floor(counter / 100);
+  const two = Math.floor(counter / 10);
+  const one = Math.floor(counter / 1);
+  console.log(four, three, two, one);
+  counter++;
+  //render your react application
+  ReactDOM.render(
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
+    document.querySelector("#app")
+  );
+}, 1000);
